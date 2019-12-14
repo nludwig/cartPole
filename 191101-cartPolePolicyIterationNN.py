c3705246ae94dbad8b3f1eafeaee4ed905a3813a
N <nick.b.ludwig@gmail.com>
2019-12-14 09:47:10 -0800

    added baseline via value estimating NN

--- 191101-cartPolePolicyIterationNN.py
+++ 191101-cartPolePolicyIterationNN.py
@@ -8,12 +8,12 @@ import torch.nn.functional as F
 import gym
 
 class Net(nn.Module):
-    def __init__(self, nInner=2, width=100):
+    def __init__(self, lIn=4, nInner=2, width=100, lOut=2):
         super().__init__() #??
         self.nInner=nInner
-        self.fcIn=nn.Linear(4, width)
+        self.fcIn=nn.Linear(lIn, width)
         self.fcInner=[nn.Linear(width, width) for _ in range(self.nInner)]
-        self.fcOut=nn.Linear(width, 2)
+        self.fcOut=nn.Linear(width, lOut)
 
         self.sm=nn.Softmax(dim=1)
         self.d1d=nn.Dropout(p=0.1)
@@ -23,7 +23,6 @@ class Net(nn.Module):
         for i in range(self.nInner):
             x=F.relu(self.fcInner[i](x))
         x=self.fcOut(x)
-        #x=self.sm(self.fcOut(x))
         return x
 
     def forwardTrain(self, x):
@@ -56,39 +55,6 @@ def compute1dIndex(index,shape):
         multiplier*=shape[i]
     return index1d
 
-def runEpisode(env,sco,episode,nEpisodes,maxEpisodeLength,seed=None,render=False):
-    rng=np.random.RandomState(seed=seed)
-    envSeed=rng.randint(np.iinfo(np.int).max)
-    env.seed(envSeed)
-    scoSeed=rng.randint(np.iinfo(np.int32).max)
-    sco.rng.seed(scoSeed)
-    obs=env.reset()
-    rew=1
-    for t in range(maxEpisodeLength):
-        if render:
-            env.render()
-        ttobs=torch.tensor(obs)[None,:].float()
-        actV=sharedArgs['net'].forwardTest(ttobs)
-        actP=sharedArgs['net'].sm(actV)[0]
-        act=sco.actionFromProbabilities(actP)
-        #actIndArr[episode,t]=act
-        index=compute1dIndex((episode,t),sharedArgs['actIndShape'])
-        sharedArgs['actInd'][index]=act
-        #obsArr[episode,t]=obs
-        index=compute1dIndex((episode,t,0),sharedArgs['obsShape'])
-        for i in range(sharedArgs['obsShape'][-1]):
-            sharedArgs['obs'][index+i]=obs[i]
-        #rewArr[episode,t]=rew
-        index=compute1dIndex((episode,t),sharedArgs['rewShape'])
-        sharedArgs['rew'][index]=int(rew)
-        obs,rew,fin,inf=env.step(act)
-        if fin:
-            #durArr[episode]=t+1
-            index=compute1dIndex((episode,),sharedArgs['durShape'])
-            sharedArgs['dur'][index]=t+1
-            break
-    #return actIndArr,obsArr,rewArr,durArr
-
 sharedArgs={}
 def loadSharedArgs(net,
                    actIndRaw,actIndShape,
@@ -105,7 +71,37 @@ def loadSharedArgs(net,
     sharedArgs['dur']=durRaw
     sharedArgs['durShape']=durShape
 
-def runEpisodeSet(env,nEpisodes,maxEpisodeLength,net,sco,lossFn,discountFactor=1.,train=True,render=True,learningRate=1e-2,rngSeed=None,nProcs=mp.cpu_count()//2):
+def runEpisode(env,sco,episode,nEpisodes,maxEpisodeLength,seed=None,render=False):
+    rng=np.random.RandomState(seed=seed)
+    envSeed=rng.randint(np.iinfo(np.int).max)
+    env.seed(envSeed)
+    scoSeed=rng.randint(np.iinfo(np.int32).max)
+    sco.rng.seed(scoSeed)
+    obs=env.reset()
+    rew=1
+    for t in range(maxEpisodeLength):
+        if render:
+            env.render()
+        ttobs=torch.tensor(obs)[None,:].float()
+        actV=sharedArgs['net'].forwardTest(ttobs)
+        actP=sharedArgs['net'].sm(actV)[0]
+        act=sco.actionFromProbabilities(actP)
+        index=compute1dIndex((episode,t),sharedArgs['actIndShape'])
+        sharedArgs['actInd'][index]=act
+        index=compute1dIndex((episode,t,0),sharedArgs['obsShape'])
+        for i in range(sharedArgs['obsShape'][-1]):
+            sharedArgs['obs'][index+i]=obs[i]
+        index=compute1dIndex((episode,t),sharedArgs['rewShape'])
+        sharedArgs['rew'][index]=int(rew)
+        obs,rew,fin,inf=env.step(act)
+        if fin:
+            index=compute1dIndex((episode,),sharedArgs['durShape'])
+            sharedArgs['dur'][index]=t+1
+            break
+
+def runEpisodeSet(env,nEpisodes,maxEpisodeLength,policyNet,policyLossFn,valueNet,
+                  valueLossFn,sco,discountFactor=1.,train=True,render=True,
+                  learningRate=1e-2,rngSeed=None,nProcs=mp.cpu_count()//2):
     err=sys.stderr
     rng=np.random.RandomState(seed=rngSeed)
 
@@ -118,7 +114,7 @@ def runEpisodeSet(env,nEpisodes,maxEpisodeLength,net,sco,lossFn,discountFactor=1
     obsRaw=mp.RawArray('d',obsShape[0]*obsShape[1]*obsShape[2])
     durShape=(nEpisodes,)
     durRaw=mp.RawArray('i',durShape[0])
-    initArgs=(net,
+    initArgs=(policyNet,
               actIndRaw,actIndShape,
               rewRaw,rewShape,
               obsRaw,obsShape,
@@ -139,37 +135,58 @@ def runEpisodeSet(env,nEpisodes,maxEpisodeLength,net,sco,lossFn,discountFactor=1
     durArr=np.frombuffer(durRaw,dtype=np.int32).reshape(durShape)
 
     if train is True: #training can only happen after have trj return
-        optimizer=torch.optim.Adam(net.parameters(), lr=learningRate)
-        optimizer.zero_grad()
+        valueOptimizer=torch.optim.SGD(valueNet.parameters(), lr=learningRate)
+        valueOptimizer.zero_grad()
+        policyOptimizer=torch.optim.Adam(policyNet.parameters(), lr=learningRate)
+        policyOptimizer.zero_grad()
+        #roll episodes into mini-batch to "vectorize"
         ret=[]
         obs=[]
         actInd=[]
         for episode in range(nEpisodes):
-            ret+=[rewArr[episode,t:durArr[episode]].sum() for t in range(durArr[episode])]
+            ret+=[rewArr[episode,t:durArr[episode]].sum() 
+                  for t in range(durArr[episode])]
             obs+=list(obsArr[episode,:durArr[episode]])
             actInd+=list(actIndArr[episode,:durArr[episode]])
         ret=torch.tensor(ret)
         obs=torch.tensor(obs).float()
         actInd=torch.tensor(actInd).long()
-        actV=net.forwardTest(obs)
-        loss=lossFn(actV,actInd)*ret/nEpisodes
-        loss=loss.mean()
-        loss.backward()
-        optimizer.step()
-    return net,actIndArr,rewArr,obsArr,durArr
+        #update value estimator net & compute val estmates
+        val=valueNet.forwardTest(obs)
+        valueLoss=valueLossFn(val,ret.float()[:,None])
+        valueLoss.backward()
+        valueOptimizer.step()
+        val=valueNet.forwardTest(obs)[:,0]
+        #compute losses & update NN
+        actV=policyNet.forwardTest(obs)
+        policyLoss=policyLossFn(actV,actInd)*(ret-val)/nEpisodes
+        policyLoss=policyLoss.mean()
+        policyLoss.backward()
+        policyOptimizer.step()
+    return policyNet,valueNet,actIndArr,rewArr,obsArr,durArr
 
-def main(nInner=1,width=32,nTrainingEpochs=100,nTrainingEpisodes=5000,maxTrainingEpisodeLength=500,nTestEpochs=10,nTestEpisodes=1,maxTestEpisodeLength=500,discountFactor=1.0,envSeed=None,render=False,rngSeed=None,fOutNmTemplate='cartPoleRewardDistribution'):
+def main(nInnerPolicy=1,widthPolicy=32,nInnerValue=1,widthValue=32,
+         nTrainingEpochs=100,nTrainingEpisodes=500,maxTrainingEpisodeLength=500,
+         nTestEpochs=10,nTestEpisodes=1,maxTestEpisodeLength=500,discountFactor=1.0,
+         envSeed=None,render=False,rngSeed=None,
+         fOutNmTemplate='cartPoleRewardDistribution'):
     err=sys.stderr
     env=gym.make('CartPole-v1')
     print(env.seed(envSeed),file=err)
-    net=Net(nInner=nInner,width=width)
+    policyNet=Net(lIn=4,nInner=nInnerPolicy,width=widthPolicy,lOut=2)
+    policyLossFn=nn.CrossEntropyLoss(reduction='none') #need to weight by ret before reducing
+    valueNet=Net(lIn=4,nInner=nInnerValue,width=widthValue,lOut=1)
+    valueLossFn=nn.MSELoss()
     sco=StochasticChooseOutput(seed=rngSeed)
-    lossFn=nn.CrossEntropyLoss(reduction='none')
-    print('nInner, width: {}, {}'.format(nInner,width),file=err)
+    print('nInnerPolicy, widthPolicy: {}, {}'.format(nInnerPolicy,widthPolicy),file=err)
+    print('nInnerValue, widthValue: {}, {}'.format(nInnerValue,widthValue),file=err)
     print('#ep\ttFmean\tmin\tmax')
 
     for n in range(0,nTrainingEpochs):
-        net,actArr,rewArr,obsArr,durArr=runEpisodeSet(env,nTrainingEpisodes,maxTrainingEpisodeLength,net,sco,lossFn,train=True,render=render)
+        policyNet,valueNet,actArr,rewArr,obsArr,durArr= \
+                runEpisodeSet(env,nTrainingEpisodes,maxTrainingEpisodeLength,
+                              policyNet,policyLossFn,valueNet,valueLossFn,sco,
+                              train=True,render=render)
         #basic stats
         print('{}\t{}\t{}\t{}'.format(n,durArr.mean(),durArr.min(),durArr.max()))
         print('{}\t{}\t{}\t{}'.format(n,durArr.mean(),durArr.min(),durArr.max()),file=err)
@@ -181,9 +198,12 @@ def main(nInner=1,width=32,nTrainingEpochs=100,nTrainingEpisodes=5000,maxTrainin
         #    for i in range(len(durVals)):
         #        print('{}\t{}\t{}'.format(durVals[i],durNums[i],durNums[i]/nTrainingEpisodes),file=f)
 
-    print('test:')
+    print('\ntest:\n')
     for n in range(0,nTestEpochs):
-        net,actArr,rewArr,obsArr,durArr=runEpisodeSet(env,nTrainingEpisodes,maxTrainingEpisodeLength,net,sco,lossFn,train=False,render=render)
+        policyNet,valueNet,actArr,rewArr,obsArr,durArr= \
+                runEpisodeSet(env,nTestEpisodes,maxTestEpisodeLength,
+                              policyNet,policyLossFn,valueNet,valueLossFn,sco,
+                              lossFn,train=False,render=render)
         print('{}\t{}\t{}\t{}'.format(n,durArr.mean(),durArr.min(),durArr.max()))
         print('{}\t{}\t{}\t{}'.format(n,durArr.mean(),durArr.min(),durArr.max()),file=err)
 
